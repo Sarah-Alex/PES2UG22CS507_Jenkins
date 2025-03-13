bd748f75a5a29980af3aa4664e215355dc1402b4
Sarah-Alex <itssarahalex@gmail.com>
2025-03-14 01:39:27 +0530

    made new cpp file

---
+++ main/sarahs.cpp
@@ -0,0 +1,10 @@
+//File: sarahs.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, sarah have successfully built and run this";
+  return 0;
+}
